3cad750e0b55212c8d3941cfe6d4440f7aa1678d
Luis Calderon <lacr2591@gmail.com>
2020-09-11 17:21:50 -0500

    Creacion de capas, clases e interfaces, implementacion de GestorDAO

---
+++ CapaDominio/Contratos/IGestorDAO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapaDominio.Contratos
+{
+    public  interface IGestorDAO
+    {
+        void AbrirConexion();
+        void CerrarConexion();
+        void IniciarTransaccion();
+        void TerminarTransaccion();
+        void CancelarTransaccion();
+    }
+}
---
+++ CapaDominio/Entidades/Juego.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapaDominio.Entidades
+{
+    public class Juego
+    {
+    }
+}
